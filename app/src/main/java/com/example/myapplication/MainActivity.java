5e54ccdf5f1dc8f5a1fc744986685f581cbd9517
kimyd34 <tommykimyd98@gmail.com>
2021-06-14 13:03:16 +0900

    textwatcher을 이용한 글자수 제한

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Editable a;
+    EditText editText;
+    TextWatcher watcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InputFilter.LengthFilter filter = new InputFilter.LengthFilter(80);
+
+        editText = findViewById(R.id.et);
+
+        watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int b = editText.getText().length();
+                a = editText.getText();
+                //editable과 edittext를 연결해도 editable의 변화가 edittext에 영향을 미친다.
+                if(b>10) {
+                    a.delete(b - 2, b - 1);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        editText.addTextChangedListener(watcher);
+
     }
 }
