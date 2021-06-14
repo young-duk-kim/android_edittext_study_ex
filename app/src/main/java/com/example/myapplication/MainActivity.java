0e2e4003affa7285c8cd3a445495643eecda40e3
kimyd34 <tommykimyd98@gmail.com>
2021-06-14 13:40:05 +0900

    setFilters를 이용한 edittext 글자수 제한

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,33 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        InputFilter.LengthFilter filter = new InputFilter.LengthFilter(80);
 
         editText = findViewById(R.id.et);
-
-        watcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                int b = editText.getText().length();
-                a = editText.getText();
-                //editable과 edittext를 연결해도 editable의 변화가 edittext에 영향을 미친다.
-                if(b>10) {
-                    a.delete(b - 2, b - 1);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        };
-
-        editText.addTextChangedListener(watcher);
-
+        InputFilter filter = new InputFilter.LengthFilter(10);
+        InputFilter[] filters = new InputFilter[]{filter};
+        editText.setFilters(filters);
     }
 }
